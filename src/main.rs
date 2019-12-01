c6f891890ccccb18ee75cc38c574081101315ffd
Ezhik <i@ezhik.me>
2019-12-01 23:31:15 +0900

    Day 1 - Part 2

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,18 @@ fn main() {
     let mut sum = 0;
 
     for line in io::BufReader::new(io::stdin()).lines() {
-        let num: i64 = line.unwrap().parse().unwrap();
-        sum += (num / 3) - 2;
+        let mut num: i64 = line.unwrap().parse().unwrap();
+        num = calc(num);
+
+        while num >= 0 {
+            sum += num;
+            num = calc(num);
+        }
     }
 
     println!("{}", sum);
+}
+
+fn calc(num: i64) -> i64 {
+    (num / 3) - 2
 }
