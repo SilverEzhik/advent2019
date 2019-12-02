cbdbdf88c0f8be3a70d6534131e5d76d9fbd1be6
Ezhik <i@ezhik.me>
2019-12-02 22:33:34 +0900

    Day 2 - Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,58 @@
-use std::io;
-use std::io::BufRead;
+use std::io::{stdin, BufRead};
+use std::str;
 
 fn main() {
-    let mut sum = 0;
+    let mut memory: Vec<usize> = vec![];
+    let mut buf: Vec<u8> = vec![];
 
-    for line in io::BufReader::new(io::stdin()).lines() {
-        let mut num: i64 = line.unwrap().parse().unwrap();
-        num = calc(num);
+    let stdin = stdin();
+    let mut stdin = stdin.lock();
 
-        while num >= 0 {
-            sum += num;
-            num = calc(num);
+    loop {
+        match stdin.read_until(b',', &mut buf) {
+            Ok(0) => break,
+            Ok(_) => {
+                if buf.last() != Some(&b',') {
+                    buf.push(b',');
+                }
+                memory.push(
+                    str::from_utf8(&buf[..buf.len() - 1])
+                        .unwrap()
+                        .trim()
+                        .trim_matches(',')
+                        .parse()
+                        .unwrap(), // oof forced unwraps
+                );
+                buf.clear();
+            }
+            _ => panic!("it broke"),
+        }
+    }
+    // 1202 challenge thing
+    memory[1] = 12;
+    memory[2] = 2;
+
+    let mut index = 0;
+    while index < memory.len() {
+        match memory[index] {
+            1 => {
+                let m1 = memory[index + 1];
+                let m2 = memory[index + 2];
+                let m3 = memory[index + 3];
+                memory[m3] = memory[m1] + memory[m2];
+                index += 4;
+            }
+            2 => {
+                let m1 = memory[index + 1];
+                let m2 = memory[index + 2];
+                let m3 = memory[index + 3];
+                memory[m3] = memory[m1] * memory[m2];
+                index += 4;
+            }
+            99 => break,
+            _ => unimplemented!(),
         }
     }
 
-    println!("{}", sum);
-}
-
-fn calc(num: i64) -> i64 {
-    (num / 3) - 2
+    println!("{}", memory[0]);
 }
